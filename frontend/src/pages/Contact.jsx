80adfa65cc8c1135bc01a00170468c712f0e05a8
Mayankk2504 <mayankkapoormk1@gmail.com>
2025-06-08 00:00:53 +0530

    All pages added and navbar updated

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Contact = () => {
   return (
     <div>
-      hello
+      Conatct
     </div>
   )
 }
 
-export default Home
+export default Contact
